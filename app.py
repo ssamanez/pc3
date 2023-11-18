84ff6a942f1c6ba563b52a45c84fd7bd1d6d451e
ssamanez <86541581+ssamanez@users.noreply.github.com>
2023-11-18 08:17:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,30 @@ DETAILS_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php"
 # Ruta principal para mostrar la lista de cocktails
 @app.route('/')
 def cocktail_list():
+    # Realizar la solicitud para obtener la lista de cocktails
     response = requests.get(API_URL)
-    data = response.json()
-    cocktails = data.get('drinks', [])
-    return render_template('cocktail_list.html', cocktails=cocktails)
+    
+    if response.status_code == 200:
+        data = response.json()
+        cocktails = data.get('drinks', [])
+        return render_template('cocktail_list.html', cocktails=cocktails)
+    else:
+        # Manejar el caso en que la solicitud no sea exitosa
+        return render_template('error.html', error_message=f"Error al obtener la lista de cocktails. Código de estado: {response.status_code}")
 
 # Ruta para mostrar los detalles de un cocktail
 @app.route('/cocktail/<int:cocktail_id>')
 def cocktail_detail(cocktail_id):
+    # Realizar la solicitud para obtener detalles de un cocktail por ID
     response = requests.get(f"{DETAILS_API_URL}?i={cocktail_id}")
-    data = response.json()
-    cocktail = data.get('drinks', [])[0] if data.get('drinks') else None
-    return render_template('cocktail_detail.html', cocktail=cocktail)
+    
+    if response.status_code == 200:
+        data = response.json()
+        cocktail = data.get('drinks', [])[0] if data.get('drinks') else None
+        return render_template('cocktail_detail.html', cocktail=cocktail)
+    else:
+        # Manejar el caso en que la solicitud no sea exitosa
+        return render_template('error.html', error_message=f"Error al obtener detalles del cocktail. Código de estado: {response.status_code}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=7000,debug=True)
